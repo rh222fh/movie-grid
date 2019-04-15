ec99882419943713befe2bc6ea8fd1898dc4273a
rh222fh <rh222fh@student.lnu.se>
2019-04-15 02:03:13 +0200

    Updates

---
+++ precache-manifest.a77a03113785544fee76b9c4fd77adbf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "51e913d5fa2f8c652c4a3dc112aa03b0",
+    "url": "/movie-grid/static/media/placeholder.51e913d5.png"
+  },
+  {
+    "revision": "c5116ed1ae2a5b7636cd",
+    "url": "/movie-grid/static/js/runtime~main.f050e970.js"
+  },
+  {
+    "revision": "7221ea225e6ddae08d7e",
+    "url": "/movie-grid/static/js/main.c9c0b886.chunk.js"
+  },
+  {
+    "revision": "c9c624f9b50e1921eca2",
+    "url": "/movie-grid/static/js/2.d315a0f0.chunk.js"
+  },
+  {
+    "revision": "7221ea225e6ddae08d7e",
+    "url": "/movie-grid/static/css/main.4bb426fb.chunk.css"
+  },
+  {
+    "revision": "c9c624f9b50e1921eca2",
+    "url": "/movie-grid/static/css/2.5c5a9cc4.chunk.css"
+  },
+  {
+    "revision": "5334f0f1635c6e079466815f2a82bc2c",
+    "url": "/movie-grid/index.html"
+  }
+];
